db940bd1d5b4b16d9f1f8d0a0ad31ae0270a8e97
Reda ZOUHAIRI <zouhairireda@gmail.com>
2017-03-26 22:51:50 +0200

    Create TokenService for communication between Resource Server and Authorization Server

---
+++ src/main/java/com/projects/security/resources/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.projects.security.resources.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jbdc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
+	}
+
+}
